58f7fd4979c58c7e676e958286213c585d951539
Linus Lee <linus@thesephist.com>
2022-04-29 10:51:57 -0400

    feat: Write script to auto-refresh dataset; refresh dataset

--- refresh_yc_data.sh
+++ refresh_yc_data.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# download the YC list from github.com/akshaybhalotia/yc_company_scraper
+wget 'https://github.com/akshaybhalotia/yc_company_scraper/raw/main/data/yc_essential_data.json' -O data/yc.json
 
+# virtualenv
+source .venv/bin/activate
+
+# re-generate embeddings
+python server/generate.py
 
--- server/generate.py
+++ server/generate.py
@@ -7,13 +7,13 @@ model = SentenceTransformer('sentence-transformers/all-roberta-large-v1')
 
 with open('data/yc.json', 'r') as yc_file:
     yc_companies = json.load(yc_file)
-    for co in yc_companies:
-        print(co['name'])
-        description = co.get('long_description')
+    for i, co in enumerate(yc_companies):
+        print(f'{i}/{len(yc_companies)}', co['name'])
+        description = co.get('long_description') or co.get('one_liner')
         if not description:
-            print(f'{co["name"]} has no description')
+            print(f'{co["name"]} has no long_description or one_liner, skipping semantic indexing')
             continue
-        embedding = model.encode(description)
+        embedding = model.encode(description.strip())
         co['description_embedding'] = embedding.tolist()
 
     with open('data/yc-embedded.json', 'w') as embeddings_file:
